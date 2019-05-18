6d1dfff6b089c13a077c92b9aa1b7b1e58cee300
Jonathan Daly <dalyj138@gmail.com>
2019-05-18 12:41:32 -0400

    Added PUT request in actionRouter.js.

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -23,4 +23,21 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.put('/:id', async (req, res) => {
+    try {
+        const changed = await ACTION_DB.update(req.params.id, req.body);
+        if(!changed) {
+            res.status(404).json({ message: 'Unable to update the action with that ID' });
+            return null;
+        } else {
+            res.status(200).json(changed);
+        }
+    } catch(err) {
+        console.log(err);
+        res.status(500).json({ message: 'Error updating resource' });
+    }
+});
+
+// router.delete();
+
 module.exports = router;
